31c490d851009b832ff558d04ca5b8ab4f63edc3
fico <fico@maher>
2019-11-11 21:56:30 +0100

    initial commit

--- cas21/Program.cs
+++ cas21/Program.cs
@@ -10,6 +10,16 @@ namespace cas21
     {
         static void Main(string[] args)
         {
+            int[] niz = new int[10];
+            double[] poeni = new double[2] { 43.332, 32.91 };
+
+            for(int i = 0; i < 10; i++)
+            {
+                niz[i] = i * 10;
+                Console.WriteLine("Vrijednost elementa niz[{0}]={1}", i, niz[i]);
+            }
+            Console.ReadKey();
         }
     }
-}
+    }
+
